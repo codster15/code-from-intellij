14e4258583ff9b71ca3616a9c8638c17edb488e4
Codster dev <deepaksingh.desire@gmail.com>
2024-01-03 19:35:02 +0530

    upload

--- Core java/List of Programs/Program Collection/src/welcome.java
+++ Core java/List of Programs/Program Collection/src/welcome.java
@@ -1,5 +1,7 @@
 public class welcome {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
             System.out.println("Hello World!");
+        System.out.println("Code is running");
     }
 }
